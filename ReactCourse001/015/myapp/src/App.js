c7e998936c4b0b1348ace4f1a1c67abf69ded791
Diego Passos <diegodospassosweb@gmail.com>
2022-06-03 12:45:57 -0300

    update react

---
+++ ReactCourse001/015/myapp/src/App.js
@@ -0,0 +1,13 @@
+import Main from './components/Main';
+import './components/style.css'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <Main />
+    </div>
+  );
+}
+
+export default App;
