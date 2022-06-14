fc03cacb2a252d46fc8a2dacbb7b521251241549
Diego Passos <diegodospassosweb@gmail.com>
2022-06-14 14:14:40 -0300

    update login

---
+++ ReactCourse001/019/login/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+// import LoginPage from './pages/LoginPage';
+// import HomePage from './pages/HomePage';
+import AppRoutes from './AppRoutes';
+
+function App() {
+  return (
+    <div className="App">
+      <AppRoutes />
+    </div>
+  );
+}
+
+export default App;
--- ReactCourse001/019/login/src/pages/LoginPage/index.jsx
+++ ReactCourse001/019/login/src/pages/LoginPage/index.jsx
@@ -1,7 +1,9 @@
-import React,{useState} from "react";
+import React,{useState, useContext} from "react";
+import { AuthContext } from "../../contexts/auth";
 import './styles.css';
 
 const LoginPage = () => {
+    const {authenticated, login} = useContext(AuthContext);
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
@@ -10,11 +12,13 @@ const LoginPage = () => {
         e.preventDefault();
 
         console.log('submit', {email, password});
+        login(email,password); // integraçao com o texto / api
     };
 
     return (
         <div id="login">
             <h1 className="title">Login do sistema</h1>
+            <p>{String(authenticated)}</p>
             <form className="form" onSubmit={handleSubmit}>
                 <div className="field">
                     <label htmlFor="email">Email</label>
