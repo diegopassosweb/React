de5ae463953552d3e7b4294fcf3a8e98e5f8a993
Diego Passos <diegodospassosweb@gmail.com>
2022-06-17 14:34:11 -0300

    update react

---
+++ ReactCourse001/018/my-app/src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import "./App.css";
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: "1",
+      title: "Estudar programaçao",
+      completed: false,
+    },
+    {
+      id: "2",
+      title: "Ler livros",
+      completed: true,
+    },
+  ]);
+
+  return <div className="container">
+    <AddTask />
+    <Tasks tasks={tasks}/>
+  </div>;
+};
+export default App;
