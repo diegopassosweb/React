8023cd24b21e6dc6d7b35b1768e03ccafc72ff8e
Diego Passos <diegodospassosweb@gmail.com>
2022-07-06 10:02:51 -0300

    react app

---
+++ ReactCourse001/025/myapp2/src/App.js
@@ -0,0 +1,12 @@
+import Home from "./pages/Home";
+import Pages from "./pages/Pages";
+
+function App() {
+  return (
+    <div className="App">
+     <Pages />
+    </div>
+  );
+}
+
+export default App;
