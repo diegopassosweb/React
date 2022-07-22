6949b02d95f0230b0a05423806a9bc5c7bc0b80e
Diego Passos <diegodospassosweb@gmail.com>
2022-07-22 11:56:27 -0300

    react login

---
+++ ReactCourse001/026/myapp/src/Register.js
@@ -0,0 +1,60 @@
+import {useRef, useState, useEffect} from "react"
+import {faCheck} from "@fortawesome/free-solid-svg-icons"
+import { faFontAwesome } from "@fortawesome/free-solid-svg-icons"
+
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
+const REGISTER_URL = '/register';
+
+
+const Register = () => {
+    const useRef = useRef();
+    const errRef = useRef();
+
+    const [user, setUser] = useState("");
+    const [validName, setValidName] = useState(false);
+    const [userFocus, setUserFocus] = useState(false);
+
+    const [pwd, setPwd] = useState("");
+    const [validPwd, setValidPwd] = useState(false);
+    const [pwdFocus, setPwdFocus] = useState(false);
+
+    const [matchPwd, setMatchPwd] = useState("");
+    const [validMatch, setValidMatch] = useState(false);
+    const [matchFocus, setMatchFocus] = useState(false);
+
+    const [errMsg, setErrMsg] = useState("");
+    const [sucess, setSucess] = useState(false);
+
+    useEffect(() => {
+        useRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        const result = USER_REGEX.test(user);
+        console.log(result);
+        console.log(user);
+        setValidName(result);
+    }, [user])
+
+    useEffect(() => {
+        const result = PWD_REGEX.test(pwd);
+        console.log(result);
+        console.log(pwd);
+        setValidPwd(result);
+        const match = pwd === matchPwd;
+        setValidMatch(match);
+    }, [pwd, matchPwd])
+
+    useEffect(() => {
+        setErrMsg("");
+    }, [user, pwd, matchPwd])
+
+  return (
+      <section>
+          <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+      </section>
+  );
+};
+
+export default Register;
