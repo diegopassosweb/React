32a7ae8d1043978f87a6246f41e16d21a97f6c49
Diego Passos <diegodospassosweb@gmail.com>
2022-07-28 12:01:20 -0300

    react + typescript basic

---
+++ ReactCourse001/028/myapp/src/components/ListItem/index.tsx
@@ -0,0 +1,20 @@
+import * as C from "./styles"
+import {Item} from "../../types/item"
+import { useState } from "react"
+
+type Props = {
+    item: Item
+}
+
+export const ListItem = ({item}: Props) => {
+    const [isChecked, setisChecked] = useState(item.done);
+    return (
+        <C.Container done={isChecked}>
+            <input type="checkbox" checked={isChecked}
+            onChange={e => setisChecked(e.target.checked)}
+            />
+
+            <label>{item.name}</label>
+        </C.Container>
+    )
+}
