f59894bfe2aeaf49ddb618487a84b7a6212c374b
Diego Passos <diegodospassosweb@gmail.com>
2022-08-05 10:04:32 -0300

    delete

--- ReactCourse001/032/myapp/src/pages/FormStep1/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as C from "./styles";
-
-export const FormStep1 = () => {
-    return (
-        <C.Container>
-            passo1
-        </C.Container>
-    )
-}
--- ReactCourse001/032/myapp/src/pages/FormStep3/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as C from "./styles";
-
-export const FormStep3 = () => {
-    return (
-        <C.Container>
-            PASSO 3
-        </C.Container>
-    )
-}
