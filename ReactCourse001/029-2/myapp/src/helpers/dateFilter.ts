bb66fbfdd5938a6280e9e540531b1f2790fa1f61
Diego Passos <diegodospassosweb@gmail.com>
2022-08-10 14:40:31 -0300

    react + typescript

--- ReactCourse001/029-2/myapp/src/helpers/dateFilter.ts
+++ ReactCourse001/029-2/myapp/src/helpers/dateFilter.ts
@@ -20,3 +20,24 @@ export const filterListByMonth = (list: Item[], date: string): Item[] => {
     return newList;
 
 }
+
+export const formatDate = (date: Date): string => {
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+
+    return ` ${day}/${month}/${year}`
+}
+
+const addZeroToDate = (n: number): string => n < 10 ? `0${n}` : `${n}`;  
+
+export const formatCurrentMonth = (currentMonth: string): string =>  {
+    let [year, month] = currentMonth.split("-");
+    let months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+    return `${months[parseInt(month) - 1]} de ${year}`;
+}
+
+export const newDateAdjusted = (dateField: string) => {
+    let [year, month, day] = dateField.split('-')
+    return new Date(parseInt(year), parseInt(month) - 1, parseInt(day))
+  }
