67ff056d92a164e3e9e0bfaed98821357a7155fb
Diego Passos <diegodospassosweb@gmail.com>
2022-09-20 13:10:28 -0300

    finanças 09

--- ReactCourse001/028-09/myapp/src/App.tsx
+++ ReactCourse001/028-09/myapp/src/App.tsx
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useEffect, useState } from "react";
+import * as C from "./App.styles";
+import { Item } from "./types/Item";
+import { items } from "./data/items";
+import { Category } from "./types/Category";
+import { categories } from "./data/categories";
+import { filterListByMonth, getCurrenthMonth } from "./helpers/dateFilter";
+import TableArea from "./components/TableArea";
+
+const App = () => {
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currenthMonth, setCurrenthMonth] = useState(getCurrenthMonth);
+
+  useEffect(() => {
+    setFilteredList(filterListByMonth(list, currenthMonth))
+  },[list,currenthMonth])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>Sistema Financeiro</C.HeaderText>
+      </C.Header>
+      <C.Body>
+
+        <TableArea list={filteredList} />
+
+
+      </C.Body>
+    </C.Container>
+  )
+};
 
 export default App;
---
+++ ReactCourse001/028-09/myapp/src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from "../types/Item";
+
+export const items: Item[] = [
+    {date: new Date(2022, 8,15), category: "food", title: "McDonalds", value: 32.12},
+    {date: new Date(2022,8,16), category: "food", title: "BurgerKing", value: 30.45},
+    {date: new Date(2022,8, 17), category: "rent", title: "Aluguel Apt", value: 2000},
+    {date: new Date(2022, 9,30), category: "salary", title: "Salario", value: 4000},
+];
