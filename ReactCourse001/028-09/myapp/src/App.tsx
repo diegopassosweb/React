ccc6ef22fe5272dc9a0ca2cdc2f75cb4ffeef710
Diego Passos <diegodospassosweb@gmail.com>
2022-09-21 14:36:53 -0300

    finanças 09

--- ReactCourse001/028-09/myapp/src/App.tsx
+++ ReactCourse001/028-09/myapp/src/App.tsx
@@ -7,15 +7,38 @@ import { Category } from "./types/Category";
 import { categories } from "./data/categories";
 import { filterListByMonth, getCurrenthMonth } from "./helpers/dateFilter";
 import TableArea from "./components/TableArea";
+import InfoArea from "./components/InfoArea";
 
 const App = () => {
   const [list, setList] = useState(items);
   const [filteredList, setFilteredList] = useState<Item[]>([]);
   const [currenthMonth, setCurrenthMonth] = useState(getCurrenthMonth);
+  const [income, setIncome] = useState(0);
+  const [expense, setexpense] = useState(0);
 
   useEffect(() => {
     setFilteredList(filterListByMonth(list, currenthMonth))
   },[list,currenthMonth])
+
+  useEffect(() => {
+    let incomeCount = 0;
+    let expenseCount = 0;
+
+    for(let i in filteredList) {
+      if(categories[filteredList[i].category].expense) {
+        expenseCount += filteredList[i].value;
+      } else {
+        incomeCount += filteredList[i].value;
+      }
+    }
+    setIncome(incomeCount);
+    setexpense(expenseCount);
+    
+  }, [filteredList])
+
+  const handleMonthChange = (newMonth: string) => {
+    setCurrenthMonth(newMonth);
+  }
 
   return (
     <C.Container>
@@ -23,6 +46,13 @@ const App = () => {
         <C.HeaderText>Sistema Financeiro</C.HeaderText>
       </C.Header>
       <C.Body>
+
+        <InfoArea 
+        currenthMonth={currenthMonth}
+        onMonthChange={handleMonthChange}
+        income={income}
+        expense={expense}
+        />
 
         <TableArea list={filteredList} />
 
