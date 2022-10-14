9476bf95ae5e232c1e005532c41dd55701ff105d
Diego Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-10-14 07:10:13 -0300

    galeria 04

---
+++ ReactCourse001/030-04/myapp/src/App.tsx
@@ -0,0 +1,80 @@
+
+import React, { FormEvent, useEffect, useState } from "react";
+import * as C from "./App.styles";
+import PhotoItem from "./components/PhotoItem";
+import * as Photos from "./services/photos";
+import { Photo } from "./types/Photo";
+
+const App = () => {
+  const [loading, setloading] = useState(false);
+  const [photos, setPhotos] = useState<Photo[]>([]);
+  const [uploading, setuploading] = useState(false);
+
+  useEffect(() => {
+    const getPhotos = async () => {
+      setloading(true);
+      setPhotos(await Photos.getAll());
+      setloading(false);
+    }
+    getPhotos();
+  },[]);
+
+  const handleFormSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+    const file = formData.get("image") as File;
+
+    if(file && file.size > 0 ) {
+      setuploading(true);
+      let result = await Photos.insert(file);
+      setuploading(false);
+
+      if(result instanceof Error) {
+        alert(`${result.name} - ${result.message}`)
+      } else {
+        let newPhotoList = [...photos];
+        newPhotoList.push(result);
+        setPhotos(newPhotoList);
+      }
+    }
+  }
+
+  return (
+    <C.Container>
+      <C.Area>
+        <C.Header>Galeria de Fotos</C.Header>
+
+        <C.UploadForm method="POST" onSubmit={handleFormSubmit}>
+          <input type="file" name="image" />
+          <input type="submit" value="Enviar" />
+          {uploading && "Enviando..."}
+        </C.UploadForm>
+
+        {loading && 
+          <C.ScreenWarning>
+            <div className="emoji">{":D"}</div>
+            <div>Carregando...</div>
+          </C.ScreenWarning>
+        }
+
+        {!loading && photos.length > 0 && 
+          <C.PhotoList>
+            {photos.map((item,index) => (
+              <PhotoItem key={index} url={item.url} name={item.name}/>
+            ))}
+          </C.PhotoList>
+        }
+
+        {!loading && photos.length === 0 &&
+          <C.ScreenWarning>
+            <div className="emoji">{"):"}</div>
+            <div>Nao ha fotos cadastradas</div>
+          </C.ScreenWarning>
+        }
+      </C.Area>
+    </C.Container>
+  )
+};
+
+export default App;
---
+++ ReactCourse001/030-04/myapp/src/components/PhotoItem/index.tsx
@@ -0,0 +1,19 @@
+
+import React from "react";
+import * as C from "./styles";
+
+type Props = {
+    url: string;
+    name: string;
+}
+
+const PhotoItem = ({url, name}: Props) => {
+  return (
+    <C.Container>
+        <img src={url} alt={name} />
+        {name}
+    </C.Container>
+  )
+};
+
+export default PhotoItem;
