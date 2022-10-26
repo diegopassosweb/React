ce818f28a879e59fb237abcb5a57ff47d5c9cf89
Diego Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-10-26 08:29:44 -0300

    finanças 17

--- ReactCourse001/028-17/myapp/src/App.tsx
+++ ReactCourse001/028-17/myapp/src/App.tsx
@@ -8,15 +8,37 @@ import { Category } from "./types/Category";
 import { categories } from "./data/categories";
 import { filteredlistbymonth, getCurrenthMonth } from "./helpers/dateFilter";
 import TableArea from "./components/TableArea";
+import InfoArea from "./components/InfoArea";
 
 const App = () => {
   const [list, setlist] = useState(items);
   const [currenthMonth, setCurrenthMonth] = useState(getCurrenthMonth());
   const [filteredlist, setfilteredlist] = useState<Item[]>([]);
+  const [income, setincome] = useState(0);
+  const [expense, setexpense] = useState(0);
 
   useEffect(() => {
     setfilteredlist(filteredlistbymonth(list, currenthMonth));
   }, [list, currenthMonth]);
+
+  useEffect(() => {
+    let incomeCount = 0;
+    let expenseCount = 0;
+
+    for(let i in filteredlist) {
+      if(categories[filteredlist[i].category].expense) {
+        expenseCount += filteredlist[i].value;
+      } else {
+        incomeCount += filteredlist[i].value;
+      }
+    }
+    setincome(incomeCount);;
+    setexpense(expenseCount);
+  }, [filteredlist])
+
+  const handleMonthChange = (newMonth: string) => {
+    setCurrenthMonth(newMonth);
+  }
 
   return (
     <C.Container>
@@ -24,6 +46,12 @@ const App = () => {
         <C.HeaderText>Sistema Financeiro</C.HeaderText>
       </C.Header>
       <C.Body>
+
+      <InfoArea currenthMonth={currenthMonth} 
+      onMonthChange={handleMonthChange}
+      income={income}
+      expense={expense}
+      />
 
 
         <TableArea list={filteredlist} />
--- ReactCourse001/028-17/myapp/src/data/items.ts
+++ ReactCourse001/028-17/myapp/src/data/items.ts
@@ -3,6 +3,6 @@ import { Item } from "../types/Item";
 export const items: Item[] = [
     {date: new Date(2022, 9,15), category: "food", title: "McDonalds", value: 25.99 },
     {date: new Date(2022, 9,16), category: "food", title: "BurgerKing", value: 28.99},
-    {date: new Date(2022, 10,1), category: "rent", title: "Aluguel", value: 2300},
-    {date: new Date(2022, 10, 30), category: "salary", title: "Salario", value: 4000}
+    {date: new Date(2022, 9,17), category: "rent", title: "Aluguel", value: 2300},
+    {date: new Date(2022, 10, 1), category: "salary", title: "Salario", value: 4000}
 ]
