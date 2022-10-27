87bbc1d428e2bbc20419110feb894f2ee69b169e
Diego Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-10-27 07:29:14 -0300

    finanças v18

---
+++ ReactCourse001/028-18/myapp/src/components/TableArea/index.tsx
@@ -0,0 +1,35 @@
+
+import React from "react";
+import { Item } from "../../types/item";
+import * as C from "./styles";
+
+type Props = {
+    list: Item[]
+}
+
+const TableArea = ({list}: Props) => {
+  return (
+    <C.Table>
+        <thead>
+            <tr>
+                <C.TableHeadColumn width={100}>Data</C.TableHeadColumn>
+                <C.TableHeadColumn width={120}>Categoria</C.TableHeadColumn>
+                <C.TableHeadColumn>Titulo</C.TableHeadColumn>
+                <C.TableHeadColumn width={130}>Valor</C.TableHeadColumn>
+            </tr>
+        </thead>
+        <tbody>
+           {list.map((item,index) => (
+            <tr>
+                <td></td>
+                <td></td>
+                <td>{item.title}</td>
+                <td></td>
+            </tr>
+           ))}
+        </tbody>
+    </C.Table>
+  )
+};
+
+export default TableArea;
---
+++ ReactCourse001/028-18/myapp/src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from "../types/item";
+
+export const items: Item[] = [
+    {date: new Date(2022,9,1), category: "food", title: "McDonalds", value: 32.99},
+    {date: new Date(2022,9,2), category: "food", title: "BurgerKing", value: 23.99},
+    {date: new Date(2022,9,30), category: "rent", title: "Aluguel", value: 2000},
+    {date: new Date(2022,10,1), category: "salary", title: "Salario", value: 4000}
+];
