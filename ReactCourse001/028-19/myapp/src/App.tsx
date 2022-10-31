638477589886e6f5219afd8515425f9cc4536776
Diego Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-10-31 10:34:56 -0300

    finanças v19

--- ReactCourse001/028-19/myapp/src/App.tsx
+++ ReactCourse001/028-19/myapp/src/App.tsx
@@ -6,15 +6,45 @@ import { Category } from "./types/Category";
 import { categories } from "./data/categories";
 import { filterListByMonth, getCurrenthMonth } from "./helpers/dateFilter";
 import TableArea from "./components/TableArea";
+import InfoArea from "./components/InfoArea";
+import { Value } from "./components/TableItem/styles";
+import InputArea from "./components/InputArea";
 
 const App = () => {
   const [list, setlist] = useState(items);
   const [currentMonth, setCurrentMonth] = useState(getCurrenthMonth());
   const [filteredlist, setfilteredlist] = useState<Item[]>([]);
+  const [income, setincome] = useState(0);
+  const [expense, setexpense] = useState(0);
 
   useEffect(() => {
     setfilteredlist(filterListByMonth(list, currentMonth));
   }, [list, currentMonth]); //monitorar list e currentmonth
+
+  const handleMonthChange = (newMonth: string) => {
+    setCurrentMonth(newMonth);
+  }
+
+  useEffect(() => {
+    let incomeCount = 0;
+    let expenseCount = 0;
+
+    for(let i in filteredlist) {
+      if(categories[filteredlist[i].category].expense) {
+        expenseCount += filteredlist[i].value
+      } else {
+        incomeCount += filteredlist[i].value
+      }
+    }
+    setexpense(expenseCount);
+    setincome(incomeCount);
+  }, [filteredlist])
+
+  const handleAddItem = (item: Item) => {
+    let newList = [...list];
+    newList.push(item);
+    setlist(newList);
+  }
 
   return (
     <C.Container>
@@ -22,7 +52,14 @@ const App = () => {
         <C.HeaderText>Sistema Financeiro</C.HeaderText>
       </C.Header>
       <C.Body>
-          
+          <InfoArea currentMonth={currentMonth}
+          onMonthChange={handleMonthChange}
+          income={income}
+          expense={expense}
+          />
+
+          <InputArea onAdd={handleAddItem} />
+
           <TableArea list={filteredlist} />
       </C.Body>
     </C.Container>
--- ReactCourse001/028-19/myapp/src/data/items.ts
+++ ReactCourse001/028-19/myapp/src/data/items.ts
@@ -2,7 +2,7 @@ import { Item } from "../types/Item";
 
 export const items: Item[] = [
     {date: new Date(2022,9,1), category: "food", title: "McDonalds", value: 25.99},
-    {date: new Date(2022,9,15), category: "food", title: "Burgering", value: 32.99},
+    {date: new Date(2022,9,15), category: "food", title: "BurgerKing", value: 32.99},
     {date: new Date(2022,9,30), category: "rent", title: "Aluguel", value: 2000},
     {date: new Date(2022,10,1), category: "salary", title: "Salario", value: 4000}
 ];
