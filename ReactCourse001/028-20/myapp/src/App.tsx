3cdae5dd93765664f42a4128ebbe7eecac16d050
Diego Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-11-02 10:23:42 -0300

    finanças v20

--- ReactCourse001/028-20/myapp/src/App.tsx
+++ ReactCourse001/028-20/myapp/src/App.tsx
@@ -7,23 +7,61 @@ import { Item } from "./types/Item";
 import { items } from "./data/items";
 import { filteredListByMonth, getCurrentMonth } from "./helpers/dateFilter";
 import TableArea from "./components/TableArea";
+import InfoArea from "./components/InfoArea";
+import InputArea from "./components/inputArea";
+
 
 const App = () => {
 
   const [list, setlist] = useState(items);
   const [currenthMonth, setCurrentMonth] = useState(getCurrentMonth());
   const [filteredlist, setfilteredlist] = useState<Item[]>([]);
+  const [income, setincome] = useState(0);
+  const [expense, setexpense] = useState(0);
 
   useEffect(() => {
     setfilteredlist(filteredListByMonth(list, currenthMonth));
   }, [list,currenthMonth])
 
+  const handleMonthChange = (newMonth: string) => {
+    setCurrentMonth(newMonth);
+  }
+
+  useEffect(() =>{
+    let incomeCount = 0;
+    let expenseCount = 0;
+
+    for(let i in filteredlist) {
+      if(categories[filteredlist[i].category].expense) {
+        expenseCount += filteredlist[i].value
+      } else {
+        incomeCount += filteredlist[i].value
+      }
+    }
+    setincome(incomeCount);
+    setexpense(expenseCount);
+  }, [filteredlist])
+
+  const handleAddItem = (item: Item) => {
+    let newlist = [...list];
+    newlist.push(item);
+    setlist(newlist);
+  }
+
   return (
+
     <C.Container>
       <C.Header>
         <C.HeaderText>Sistema Financeiro</C.HeaderText>
       </C.Header>
       <C.Body>
+
+        <InfoArea currentMonth={currenthMonth}
+        onMonthChange={handleMonthChange}
+        income={income}
+        expense={expense}/>
+
+        <InputArea onAdd={handleAddItem} />
 
         <TableArea list={filteredlist} />
 
