e2b62c93f9f1f33bca74dc5d83145f331e3ba090
Diego Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-11-11 08:09:16 -0300

    finanças v25

--- ReactCourse001/028-25/myapp/src/App.tsx
+++ ReactCourse001/028-25/myapp/src/App.tsx
@@ -7,16 +7,39 @@ import { Category } from "./types/Category";
 import { categories } from "./data/categories";
 import { filteredListByMonth, getCurrentMonth } from "./helpers/dateFilter";
 import TableArea from "./components/TableArea";
+import InfoArea from "./components/InfoArea";
 
 const App = () => {
   
   const [list, setlist] = useState(items);
   const [currentMonth, setcurrentMonth] = useState(getCurrentMonth());
   const [filteredlist, setfilteredlist] = useState<Item[]>([]);
+  const [income,setincome] = useState(0);
+  const [expense, setexpense] = useState(0);
+
 
   useEffect(() => {
     setfilteredlist(filteredListByMonth(list,currentMonth));
   }, [list, currentMonth])
+
+  const handleMonthChange = (newMonth: string) => {
+    setcurrentMonth(newMonth);
+  }
+
+  useEffect(() => {
+    let incomeCount = 0;
+    let expenseCount = 0;
+
+    for(let i in filteredlist) {
+      if(categories[filteredlist[i].category].expense) {
+        expenseCount += filteredlist[i].value;
+      } else {
+        incomeCount += filteredlist[i].value;
+      }
+    }
+    setexpense(expenseCount);
+    setincome(incomeCount);
+  }, [filteredlist])
 
   return (
     <C.Container>
@@ -25,6 +48,11 @@ const App = () => {
       </C.Header>
       <C.Body>
 
+      <InfoArea currentMonth={currentMonth}
+      onMonthChange={handleMonthChange}
+      income={income}
+      expense={expense}
+      />
       <TableArea list={filteredlist}/>
       </C.Body>
     </C.Container>
--- ReactCourse001/028-25/myapp/src/helpers/dateFilter.ts
+++ ReactCourse001/028-25/myapp/src/helpers/dateFilter.ts
@@ -20,3 +20,19 @@ export const filteredListByMonth = (list: Item[], date: string): Item[] => {
     
     return newList;
 }
+
+export const formatDate = (date: Date): string => {
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+
+    return `${ZeroToDate(day)}/${ZeroToDate(month)}/${year}`
+}
+
+export const ZeroToDate = (n: number): string => n < 10 ? `0${n}` : `${n}`
+
+export const formatCurrentMonth = (currentMonth: string): string => {
+    let [year,month] = currentMonth.split("-");
+    let months = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"];
+    return `${months[parseInt(month) -1]} ${year}`;
+}
