5abc1d8dda7bc07f3ba54627136a497c61a39d17
Diego Passos <95578099+diegopassosweb@users.noreply.github.com>
2022-11-15 09:35:46 -0300

    finanças v27

---
+++ ReactCourse001/028-27/myapp/src/App.tsx
@@ -0,0 +1,35 @@
+
+import React, { useEffect, useState } from "react";
+import * as C from "./App.styles";
+import { Category } from "./types/Category";
+import { categories } from "./data/categories";
+import { items } from "./data/items";
+import { Item } from "./types/Items";
+import { filteredListByMonth, getCurrentMonth } from "./helpers/dateFilter";
+import TableArea from "./components/TableArea";
+
+const App = () => {
+
+  const [list, setlist] = useState(items);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+  const [filteredlist, setfilteredlist] = useState<Item[]>([]);
+
+  useEffect(() => {
+    setfilteredlist(filteredListByMonth(list, currentMonth));
+  },[list, currentMonth]);
+
+  return (
+    <C.Container>
+      <C.Header>
+        <C.HeaderText>Sistema Financeiro</C.HeaderText>
+      </C.Header>
+      <C.Body>
+
+        <TableArea list={filteredlist} />
+
+      </C.Body>
+    </C.Container>
+  )
+};
+
+export default App;
---
+++ ReactCourse001/028-27/myapp/src/data/items.ts
@@ -0,0 +1,8 @@
+import { Item } from "../types/Items";
+
+export const items: Item[] = [
+    {date: new Date(2022,10,1), category: "food", title: "McDonalds", value: 25.49},
+    {date: new Date(2022,10,2), category: "food", title: "BurgerKing", value: 35.49},
+    {date: new Date(2022,10,20), category: "rent", title: "Aluguel", value: 2000},
+    {date: new Date(2022,11,1), category: "salary", title: "Salario", value: 4000}
+]
